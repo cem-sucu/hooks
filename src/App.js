50e19e30ce19980e7436f9004f2522e13f2190a2
cem-sucu <cems.cems.17@gmail.com>
2022-08-17 16:27:56 +0200

    function state usState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import ClassStates from "./components/ClassStates";
+import FunctionState from "./components/FunctionState";
 
 function App() {
     return (
@@ -7,6 +8,8 @@ function App() {
             <header className="App-header">
                 <h1>hello</h1>
                 <ClassStates />
+                <hr />
+                <FunctionState />
             </header>
         </div>
     );
---
+++ src/components/FunctionState.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+const FunctionState = () => {
+    const [count, setCounter] = useState(7);
+
+    const addOne = () => {
+        setCounter(count + 2);
+    };
+    return (
+        <div>
+            <p>incremente : </p>
+            <button onClick={addOne}>incrémenté + 2</button>
+        </div>
+    );
+};
+
+export default FunctionState;
