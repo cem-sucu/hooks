46cca25eba4e705ee2d4aeee6a72731db38a1288
cem-sucu <cems.cems.17@gmail.com>
2022-08-18 12:34:02 +0200

    hook d'état useState suite todolist opérationnel

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import AddTodoForms from "./AddTodoForms";
 
 const Todo = () => {
     const [todos, setTodos] = useState([
@@ -7,13 +9,38 @@ const Todo = () => {
         { id: 3, todo: "acheter du chocolat" },
     ]);
 
+    const [warning, setWarning] = useState(false);
+
     const myTodos = todos.map((todo) => {
         return <li key={todo.id}>{todo.todo}</li>;
     });
+
+    const addNewTodo = (newTodo) => {
+        if (newTodo !== "") {
+            warning && setWarning(false);
+            setTodos([
+                ...todos,
+                {
+                    id: uuidv4(),
+                    todo: newTodo,
+                },
+            ]);
+        } else {
+            setWarning(true);
+        }
+    };
+
+    const warningmsg = warning && (
+        <div style={{ color: "white", backgroundColor: "red", fontSize: 30 }}>
+            Veuillez indiquer un élément !
+        </div>
+    );
     return (
         <div>
+            {warningmsg}
             <h1>{todos.length} To-Do</h1>
             <ul>{myTodos}</ul>
+            <AddTodoForms addNewTodo={addNewTodo} />
         </div>
     );
 };
